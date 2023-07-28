b42d0cb22adf468f70e80692e8b5e8024b8f4104
ingramlr <lringram00@gmail.com>
2023-07-28 13:23:24 -0600

    Assignment 6 Part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,177 +1,307 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <math.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdatomic.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <syslog.h>
-#include <unistd.h>
-#include <signal.h>
-#include <netdb.h>
-#include <fcntl.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/syslog.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#define PORT "9000"
-#define CONNECTIONS 10  //10 possible waiting connections
+#define PORT 9000
+#define BACKLOG 10
 #define FILENAME "/var/tmp/aesdsocketdata"
+#define FALSE 0
+#define TRUE 1
 
-//Declare our fds and set to -1 for no file error so they can be used globally for cleaner code
-static volatile int sock_fd = -1, client_fd = -1, file_fd = -1;         //Socket connection fd, client connection fd, and the tmp file fd
+typedef struct pthread_arg_t {
+    int new_socket_fd;
+    struct sockaddr_in client_address;
+} pthread_arg_t;
 
-void signals(){
+int socket_fd = -1; //Declare the global variables for socket and file fds
+int file_fd = -1;
+pthread_mutex_t fileFD; //Declare the mutex lock
+atomic_bool timeStamp = FALSE;
 
-    if (file_fd >= 0 && close(file_fd)) syslog(LOG_ERR, "%s: %m", "Close file"); //If a file is still open, close it and log it
+// Thread routine to serve connection to client
+void *pthread_routine(void *arg);
 
-    if (client_fd >= 0){                                                                       //If a client connection is still open, shutdown the IO, close it and log it
-        if (shutdown(client_fd, SHUT_RDWR)) syslog(LOG_ERR, "%s: %m", "Shutdown connection");
-        if (close(client_fd)) syslog(LOG_ERR, "%s: %m", "Close socket descriptor");
+//Timer setup function
+static void timerSetup();
+
+//Temporary file open
+static void tmpfileOpen();
+
+//File writing function
+void *fileWrite(char* textbuffer);
+
+// Signal handler to handle SIGTERM, SIGINT, and Timer signals
+void signal_handler(int sig, siginfo_t *si, void *uc);
+
+int main(int argc, char *argv[]) {
+    int new_socket_fd; //port
+    struct sockaddr_in address;
+    pthread_attr_t pthread_attr;
+    pthread_arg_t *pthread_arg;
+    pthread_t pthread;
+    socklen_t client_address_len;
+    static int yes = 1;
+
+    struct sigaction sa = { 0 };
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = signal_handler;
+
+    /* Initialise IPv4 address. */
+    memset(&address, 0, sizeof address);
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+    address.sin_addr.s_addr = INADDR_ANY;
+
+    // Create TCP socket
+    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        syslog(LOG_ERR, "ERROR with socket %s", strerror(errno));
+        exit(1);
     }
-
-    if (sock_fd >= 0 && close(sock_fd)) syslog(LOG_ERR, "%s: %m", "Close server descriptor");   //If the socket is still open, close it and log it
-
-    if ((unlink(FILENAME)) == -1 ) syslog(LOG_ERR, "%s: %m", "Error deleting tmp file");   //Delete the tmp file we created and log if error
-
-    if (SIGINT | SIGTERM){                                                                      //If SIGINT or SIGTERM is sent, log it and exit
-        syslog(LOG_DEBUG,"%s", "Caught signal, exiting"); 
-        exit(EXIT_SUCCESS);
-    }
-    exit(EXIT_FAILURE);
-}
-
-int main(int argc, char * argv[]){
-    int  yes = 1;   //Using "yes" from Beejs Networking
-    struct addrinfo hints, *server_addr;
-    struct sockaddr_in client_addr;
-    socklen_t addrlen = sizeof(client_addr);
-    char buffer[1024];  //Total buffer size is 1kB
-
-    // Set the server address
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    int addrerror = getaddrinfo(NULL, PORT, &hints, &server_addr);
-
-    if(addrerror != 0) syslog(LOG_ERR, "ERROR getaddrinfo: %s", gai_strerror(addrerror));
-
-    // Create socket file descriptor
-    if ((sock_fd = socket(server_addr->ai_family, server_addr->ai_socktype, server_addr->ai_protocol)) == -1){
-
-        syslog(LOG_ERR, "ERROR socket error");
-        return -1;
-    }
-
     // Set socket options to reuse port
-    if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1){
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1){
 
         syslog(LOG_ERR, "ERROR sock options");
-        close(sock_fd);
+        close(socket_fd);
         return -1;
     }
-
-    // Bind socket to port
-    if (bind(sock_fd, server_addr->ai_addr, server_addr->ai_addrlen) == -1){
-
-        syslog(LOG_ERR, "ERROR bind failed");
-        close(sock_fd);
-        return -1;
+    // Bind address to socket
+    if (bind(socket_fd, (struct sockaddr *)&address, sizeof address) == -1) {
+        syslog(LOG_ERR, "ERROR with bind: %s", strerror(errno));
+        exit(1);
     }
 
-    freeaddrinfo(server_addr); //Free the malloc called within getaddrinfo as its not needed anymore
+    // Listen on socket
+    if (listen(socket_fd, BACKLOG) == -1) {
+        syslog(LOG_ERR, "ERROR with listen");
+        exit(1);
+    }
 
-    if (argc > 1){
+    /* Initialise pthread attribute to create detached threads. */
+    if (pthread_attr_init(&pthread_attr) != 0) {
+        syslog(LOG_ERR, "ERROR with pthread attribute initialization");
+        exit(1);
+    }
+    if (pthread_attr_setdetachstate(&pthread_attr, PTHREAD_CREATE_DETACHED) != 0) {
+        syslog(LOG_ERR, "ERROR with setting pthread state");
+        exit(1);
+    }
+    if(pthread_mutex_init(&fileFD, NULL) != 0){     //pThread mutex initialization
+        syslog(LOG_ERR, "ERROR mutex init fail");
+    }
+
+    if (argc > 1){  //Argument check and daemon mode
         if (strcmp(argv[1], "-d") == 0){
             
             int pid = fork();
 
             if (pid == -1){ //Error checking with fork()
                 syslog(LOG_ERR, "ERROR fork");
-                close(sock_fd);
+                close(socket_fd);
                 return -1;
             }
             else if (pid != 0){ //Assuming fork() executes correctly we then close the main application for the daemon to exist
-                close(sock_fd);
+                close(socket_fd);
                 exit(EXIT_SUCCESS);
             }
         }
         else {
-            syslog(LOG_ERR, "ERROR Invalid argument specified");    //Just some error codes if someone/something tries anyhing other than "-d"
-            printf("ERROR Invalid argument specified\n");
+            syslog(LOG_ERR, "ERROR Invalid argument specified, running in foreground");    //Just some error codes if someone/something tries anyhing other than "-d"
+            printf("ERROR Invalid argument specified, running in foreground\n");
         }
     }
 
-    // Listen for incoming connections
-    if (listen(sock_fd, CONNECTIONS) == -1){
+    tmpfileOpen();
+    timerSetup();
 
-        syslog(LOG_ERR, "ERROR with listen");
-        close(sock_fd);
-        return -1;
+    // Assign signal handlers to signals
+    if (sigaction(SIGTERM, &sa, NULL) == -1) {
+        syslog(LOG_ERR, "ERROR with signal: %s", strerror(errno));
+        exit(1);
+    }
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        syslog(LOG_ERR, "ERROR signal: %s", strerror(errno));
+        exit(1);
+    }
+    if (sigaction(SIGRTMIN, &sa, NULL) == -1){
+        syslog(LOG_ERR, "ERROR sigaction: %s", strerror(errno));
+        exit(-1);
     }
 
-    // Set signal handlers for SIGINT and SIGTERM
-    signal(SIGINT, signals);
-    signal(SIGTERM, signals);
 
-    // Loop forever accepting incoming connections
-    ssize_t bytes_read=0;
-    while (1){
-        // Accept a new client connection
-        if ((client_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &addrlen)) == -1) { //Error checking for a -1 return value
-            syslog(LOG_ERR, "ERROR with accept");
+    while (1) {
+
+        if(timeStamp == TRUE){
+            time_t rawtime;
+            struct tm *info;
+            char *buffer = (char*)calloc(31, sizeof(char)); //Dynamically allocate the array accordingly 
+
+            time(&rawtime);
+            info = localtime(&rawtime);
+            strftime(buffer,31,"timestamp:%F %H:%M:%S\n", info);
+
+            pthread_mutex_lock(&fileFD);    //Obtain mutex lock
+            fileWrite(buffer);      //Send the buffer to the file writing function
+            pthread_mutex_unlock(&fileFD);    //Obtain mutex lock
+            syslog(LOG_DEBUG, "%s", buffer);
+
+            free(buffer);                   //Free the buffer created
+            timeStamp = FALSE;
+        }
+
+        // Create pthread argument for each connection to client
+        pthread_arg = (pthread_arg_t *)malloc(sizeof *pthread_arg); //Dynamically allocate the memory needed for a new client connection
+        if (!pthread_arg) {
+            syslog(LOG_ERR,"ERROR with pthread malloc");
             continue;
         }
 
-        // Log connection details to syslog
-        char client_ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN);    //Using this from Beejs guide
-        syslog(LOG_DEBUG, "Accepted connection from %s", client_ip);
-
-        // Read data from the client connection
-        while ((bytes_read = recv(client_fd, buffer, 1024, 0)) > 0) {   //Buffer set to 1kB
-            if (bytes_read == -1){
-                syslog(LOG_ERR, "ERROR with recv");
-                signals();
-            }
-            // Append the data to the file
-            // Open if closed
-            if (file_fd < 0) {
-                file_fd = open(FILENAME, O_CREAT | O_RDWR | O_APPEND, 0644);
-                if (file_fd < 0) {
-                    syslog(LOG_ERR, "ERROR with file open");
-                    signals();
-                }
-            }
-            // write to file
-            if ((write(file_fd, &buffer, bytes_read)) == -1){
-                syslog(LOG_ERR, "ERROR with write");
-            }
-            // exit from reading loop if
-            if (buffer[bytes_read-1] == '\n')
-                break;
+        
+        // Accept connection to client
+        client_address_len = sizeof pthread_arg->client_address;
+        new_socket_fd = accept(socket_fd, (struct sockaddr *)&pthread_arg->client_address, &client_address_len);
+        if (new_socket_fd == -1) {
+            syslog(LOG_ERR,"ERROR with accept");
+            free(pthread_arg);
+            continue;
         }
 
-        // Sending answer
-        if (lseek(file_fd, (off_t) 0, SEEK_SET) == (off_t) -1){ //Check for error with (off_t) as defined by POSIX
-            syslog(LOG_ERR, "ERROR with seek");
-            signals();
-        }
+        // Initialise pthread argument
+        pthread_arg->new_socket_fd = new_socket_fd;
 
-        int bytes_send;
-        while ((bytes_read = read(file_fd, &buffer, 1024)) > 0){    //Bytes and buffer set to 1024, 1kB
-            while ((bytes_send = send(client_fd, &buffer, bytes_read, 0)) < bytes_read){
-                syslog(LOG_ERR, "ERROR with send");
-                signals();
-            }
+        // Create thread to serve connection to client
+        if (pthread_create(&pthread, &pthread_attr, pthread_routine, (void *)pthread_arg) != 0) {
+            syslog(LOG_ERR,"ERROR with pthread_create");
+            free(pthread_arg);
+            continue;
         }
-
-        // Close connnection and log details to syslog
-        close(file_fd); //Close the tmp file
-        file_fd = -1;       //Set to -1 so no other file can be opened
-        close(client_fd);   //Close the client connection to the server
-        client_fd = -1;     //Set to -1 so no other file can be opened
-        syslog(LOG_DEBUG, "Closed connection from %s", client_ip);
     }
-    exit(EXIT_SUCCESS);
+    free(pthread_arg);
+    return 0;
+}
+
+void *pthread_routine(void *arg) {
+    pthread_arg_t *pthread_arg = (pthread_arg_t *)arg;
+    int new_socket_fd = pthread_arg->new_socket_fd;
+    struct sockaddr_in client_address = pthread_arg->client_address;
+    
+    char client_ip[INET_ADDRSTRLEN];    //Define the client IP character array
+    inet_ntop(AF_INET, &(client_address.sin_addr), client_ip, INET_ADDRSTRLEN); //Convert the client IP character array to human readable format
+    syslog(LOG_DEBUG, "Accepted connection from %s", client_ip);    //Logging who the connection was from
+
+
+    free(arg);  //Free the pthread argument buffer
+
+    ssize_t bytes_read = 0;
+    char *textbuffer = (char*)calloc(1024, sizeof(char));
+
+    // Read data from the client connection
+    while ((bytes_read = recv(new_socket_fd, textbuffer, 1024, 0)) > 0) {   //Buffer set to 1kB
+        if (bytes_read == -1){
+            syslog(LOG_ERR, "ERROR with recv");
+            raise(SIGINT);
+        }
+        
+        tmpfileOpen();  //If for some reason the file is not open, reopen it
+
+        pthread_mutex_lock(&fileFD);    //Obtain mutex lock
+        fileWrite(textbuffer);
+
+        // exit from reading loop if
+        if (textbuffer[bytes_read-1] == '\n') break;
+    }
+    if (lseek(file_fd, (off_t) 0, SEEK_SET) == (off_t) -1){ //Check for error with (off_t) as defined by POSIX
+        syslog(LOG_ERR, "ERROR with seek");
+        raise(SIGINT);
+    }
+
+    int bytes_send = 0;
+    while ((bytes_read = read(file_fd, textbuffer, 1024)) > 0){    //Bytes and buffer set to 1024, 1kB
+        while ((bytes_send = send(new_socket_fd, textbuffer, bytes_read, 0)) < bytes_read){
+            syslog(LOG_ERR, "ERROR with send");
+            raise(SIGINT);
+        }
+    }
+    pthread_mutex_unlock(&fileFD);    //Obtain mutex unlock
+
+    free(textbuffer);   //Free the allocated buffer per thread
+    close(new_socket_fd);
+    syslog(LOG_DEBUG, "Closed connection from %s", client_ip);
+    return NULL;
+}
+
+void signal_handler(int sig, siginfo_t *si, void *uc) {
+    (void)uc;   //Type cast uc to void as we dont need it
+
+    if(si->si_code == SI_TIMER){
+        timeStamp = TRUE;   //Set the global flag for the timer to true to be signal safe
+
+    } else{  //Can reasonably assume any other code is an issue
+
+        if (file_fd >= 0 && close(file_fd)) syslog(LOG_ERR, "%s: %m", "Close file"); //If a file is still open, close it and log it
+        if (socket_fd >= 0 && close(socket_fd)) syslog(LOG_ERR, "%s: %m", "Close server descriptor");   //Close the socket descritor and error if unable
+        if ((unlink(FILENAME)) == -1 ) syslog(LOG_ERR, "%s: %m", "Error deleting tmp file");   //Delete the tmp file we created and log if error
+    
+        if((sig == SIGINT) | (sig ==SIGTERM)){
+            syslog(LOG_DEBUG,"%s", "Caught signal, exiting"); 
+            exit(EXIT_SUCCESS);
+        }
+
+        exit(EXIT_FAILURE);
+    }
+}
+
+void *fileWrite(char* textbuffer){
+    if ((write(file_fd, textbuffer, strnlen(textbuffer, 1024))) == -1){
+        syslog(LOG_ERR, "ERROR with write");
+    }
+    return(0);
+}
+
+static void timerSetup(){  //Everything needed to setup the reoccuring 10s timer
+    timer_t timerId = 0;
+
+    struct sigevent sev = { 0 };
+    struct itimerspec its = {   .it_interval.tv_sec  = 10,  //Specifies the time in seconds to wait resetting automatically
+                                .it_interval.tv_nsec = 0,
+                                .it_value.tv_sec  = 1,      //If set to 0 timer is disarmed
+                                .it_value.tv_nsec = 0
+                            };
+    
+    sev.sigev_notify = SIGEV_SIGNAL; // Linux-specific
+    sev.sigev_signo = SIGRTMIN;
+
+    //Create the Timer
+    if (timer_create(CLOCK_REALTIME, &sev, &timerId) != 0){
+        syslog(LOG_ERR,"Error timer_create: %s\n", strerror(errno));
+        exit(-1);
+    }
+        
+    // start timer
+    if (timer_settime(timerId, 0, &its, NULL) != 0){
+        syslog(LOG_ERR,"Error timer_settime: %s\n", strerror(errno));
+        exit(-1);
+    }
+    free(timerId);
+}
+
+static void tmpfileOpen(){
+    if (file_fd < 0) {
+        file_fd = open(FILENAME, O_CREAT | O_RDWR | O_APPEND, 0644);
+        if (file_fd < 0) {
+            syslog(LOG_ERR, "ERROR with file open");
+            raise(SIGINT);
+        }
+    }
 }
